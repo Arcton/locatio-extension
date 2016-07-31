fe852f1e5751096482b227961b14db89c380e13e
Nathaniel Watson <nathaniel@nwatson.nz>
2016-07-31 13:24:54 +1200

    Fix hard-coded crime stats (turns out not everywhere is crime-free)

--- src/js/front/views/crime-card.js
+++ src/js/front/views/crime-card.js
@@ -33,7 +33,7 @@ export default class CrimeCard extends Card {
 
   _processData(d) {
     this.data = {
-      ratio: 0.2,
+      ratio: d[' Rate_ratio_NZ_average_rate '],
       rate: d[' Rate_per_10000_population '],
       events: d['Victimisations_calendar_year_2015'],
       name: d['Area_unit_2013_label']
